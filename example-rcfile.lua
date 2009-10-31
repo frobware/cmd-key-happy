13d027273fd8d9242c45af8ff32b57ae77cfa207
Andrew McDermott <andrew.iain.mcdermott@gmail.com>
2009-10-31 21:30:46 +0000

    updated example

--- example-rcfile.lua
+++ example-rcfile.lua
@@ -1,5 +1,3 @@
---os.delete("/tmp/foo")
-
 function Set(t)
    local s = {}
    for _,v in pairs(t) do s[v] = true end
@@ -13,40 +11,38 @@ end
 -- The set of shortcuts we don't want to swap cmd/alt.
 
 global_excludes = Set{ "shift-control-cmd-i",
+                       "shift-control-cmd-n",
                        "shift-cmd-tab",
                        "cmd-tab",
                        "shift-cmd-n",
                        "shift-cmd-e" }
 
--- The set of apps we want to consider swapping keys for with
--- exclusions.
+-- The set of apps we want to consider swapping keys for, with some
+-- notable exclusions.
 
 apps = {
    Terminal = { exclude = Set{ "shift-cmd-[",
-                               "shift-cmd-]" } },
+                               "shift-cmd-]",
+                               "cmd-w",
+                               "cmd-1",
+                               "cmd-2",
+                               "cmd-3",
+                               "cmd-t",
+                               "cmd-n",
+                               } },
    Eclipse  = { exclude = {} }
 }
 
 -- Return true to swap cmd/alt, otherwis false.
 
 function swap_keys(t)
---   for i,v in pairs(t) do print(i,v) end
+   --   for i,v in pairs(t) do print(i,v) end
    if set_contains(global_excludes, t.key_str_seq) then return false end
    if not apps[t.appname] then return false end
-   if set_contains(apps[t.appname]["exclude"], t.key_str_seq) then
+   local excludes = apps[t.appname]["exclude"]
+   if set_contains(excludes, t.key_str_seq) then
       return false
    end
    return true
 end
-
-
-
-
-
-
-
-
-
-
-
 
