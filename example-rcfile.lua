950c9d020319f16e03f1abe6b8a0e146b23692c7
Andrew McDermott <andrew.iain.mcdermott@gmail.com>
2009-11-01 10:29:48 +0000

    added some documentation to the sample lua script

--- example-rcfile.lua
+++ example-rcfile.lua
@@ -8,7 +8,7 @@ function set_contains(t, e)
    return t[e]
 end
 
--- The set of shortcuts we don't want to swap cmd/alt.
+-- The set of global shortcuts we don't want to swap cmd/alt.
 
 global_excludes = Set{ "shift-control-cmd-i",
                        "shift-control-cmd-n",
@@ -33,10 +33,24 @@ apps = {
    Eclipse  = { exclude = {} }
 }
 
--- Return true to swap cmd/alt, otherwis false.
+-- Return true to swap cmd/alt, otherwise false.
+
+-- This function is passed a table comprising the following keys:
+--
+--   key_str_seq	key sequence (e.g., "shift-cmd-e")
+--   alt		true if the alt key was pressed
+--   fn                 true if the fn key was pressed
+--   control            true if the control key was pressed
+--   shift              true if the shift key was pressed
+--   cmd                true if the command key was pressed
+--   keycode		numeric virtual keycode (e.g., 48)
+--   appname            the frontmost application (e.g., Terminal)
+--
+-- The order of the modifier keys in key-str-eq is always:
+--   shift control alt cmd fn, separated by a hypen ("-").
 
 function swap_keys(t)
-   --   for i,v in pairs(t) do print(i,v) end
+   -- for i,v in pairs(t) do print(i,v) end
    if set_contains(global_excludes, t.key_str_seq) then return false end
    if not apps[t.appname] then return false end
    local excludes = apps[t.appname]["exclude"]
@@ -45,4 +59,3 @@ function swap_keys(t)
    end
    return true
 end
-
