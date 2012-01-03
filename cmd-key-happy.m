frobware/cmd-key-happy

commit cc906e9395a1f08c15b6e2aa4deb8bc8bb8dc2ea

Handle potential NIL value when translating keycode

cmd-key-happy.m
@@ -218,6 +218,10 @@ NSString *translateKeycode(CGKeyCode keyCode, CGEventRef event)
 
     currentKeyboard = TISCopyCurrentKeyboardInputSource();
     uchr = (CFDataRef) TISGetInputSourceProperty(currentKeyboard, kTISPropertyUnicodeKeyLayoutData);
+
+    if (uchr == nil)
+        return nil;
+
     keyboardLayout = (const UCKeyboardLayout *)CFDataGetBytePtr(uchr);
     keyboardType = CGEventSourceGetKeyboardType(source);
     

commit db8febb7e1b4b40f3f111ce8239929c8eb873541

Fix leak introduced by cc906e9395a1f08c15b6e2aa4deb8bc8bb8dc2ea
This is only a potential leak.

cmd-key-happy.m
@@ -219,8 +219,10 @@ NSString *translateKeycode(CGKeyCode keyCode, CGEventRef event)
     currentKeyboard = TISCopyCurrentKeyboardInputSource();
     uchr = (CFDataRef) TISGetInputSourceProperty(currentKeyboard, kTISPropertyUnicodeKeyLayoutData);
 
-    if (uchr == nil)
+    if (uchr == nil) {
+        CFRelease(source);
         return nil;
+    }
 
     keyboardLayout = (const UCKeyboardLayout *)CFDataGetBytePtr(uchr);
     keyboardType = CGEventSourceGetKeyboardType(source);